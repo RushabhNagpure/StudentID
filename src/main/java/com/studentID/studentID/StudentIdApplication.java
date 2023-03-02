1e9ad2e921671e603c6a44ca8c141fa0e79f95a2
Rushabh <rushabhnagpure864@gmail.com>
2023-03-02 21:22:35 +0530

    add

---
+++ src/main/java/com/studentID/studentID/StudentIdApplication.java
@@ -0,0 +1,13 @@
+package com.studentID.studentID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentID/studentID/StudentIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentID.studentID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
